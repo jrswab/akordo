73316c7aa36f20db64fdb1e7b755d4a76e348844
Jaron Swab <jr@jrswab.com>
2020-04-17 13:03:57 -0400

    Added Meme and Ping to manpages and manual.go

--- manuals/manpages.go
+++ manuals/manpages.go
@@ -11,12 +11,61 @@ SYNOPSIS
        --man [command]
 
 DESCRIPTION
-       Man returns to the requester the informait about a comannd and how
+       Man returns to the requester the informait about a command and how
        that command is to be used. The message is sent as a DM to the user
        requesting the information.
 
 EXAMPLE
        --man pong
+`
+
+// Meme is the man page for `--man man`
+const Meme string = `
+MEME(1) User Commands MEME(1)
+
+NAME
+       meme - Create a meme on the fly.
+
+SYNOPSIS
+       --meme [meme name] [top text] [bottom text]
+
+DESCRIPTION
+       Meme sends back a new meme created with the text the user provides
+       by calling the memegen.link api. Text with spaces must be formatted
+       as this_is_top or this-is-top.
+
+COMMAND SYNOPSIS
+       This is just a brief synopsis of --meme commands to serve as a
+       reminder to those who already know the memegen.link api; for more
+       information please refer to https://memegen.link/api/
+
+       --meme list
+              Returns the template url on memegen.link with all available images.
+       
+       --meme spongebob why_are_you here
+              Returns the url to the newly created mocking spongebob meme with
+              the top text "why are you" and the bottom text "here".
+       
+       --meme spongebob why_are_you
+              Returns the url to the newly created mocking spongebob meme with
+              the top text "why are you" and no bottom text.
+`
+
+// Ping is the man page for `--man rule34`
+const Ping string = `
+PING(1) User Commands PING(1)
+
+NAME
+       ping - Used to check the bots responce.
+
+SYNOPSIS
+       --ping
+
+DESCRIPTION
+       Ping will return "pong" when the bot is online.
+
+EXAMPLE
+        --ping
 `
 
 // Rule34 is the man page for `--man rule34`
--- plugins/manual.go
+++ plugins/manual.go
@@ -11,7 +11,6 @@ import (
 // This is to give the user a UNIX style man page of what the
 // command is able to do.
 func Manual(req []string, s *dg.Session, msg *dg.MessageCreate) {
-	memberID := msg.Member.User.ID
 	if len(req) < 2 {
 		_, err := s.ChannelMessageSend(msg.ChannelID, "Usage: `--man <command name>`")
 		if err != nil {
@@ -21,10 +20,44 @@ func Manual(req []string, s *dg.Session, msg *dg.MessageCreate) {
 	}
 
 	switch req[1] {
-	case "rule34":
-		dm, err := s.UserChannelCreate(memberID)
+	case "man":
+		dm, err := s.UserChannelCreate(msg.Author.ID)
 		if err != nil {
-			log.Printf("s.UserChannelCreate failed to create DM for %s: %s", memberID, err)
+			log.Printf("s.UserChannelCreate failed to create DM for %s: %s",
+				msg.Author.Username, err)
+			return
+		}
+		_, err = s.ChannelMessageSend(dm.ID, man.Man)
+		if err != nil {
+			log.Printf("session.ChannelMessageSend failed: %s", err)
+		}
+	case "meme":
+		dm, err := s.UserChannelCreate(msg.Author.ID)
+		if err != nil {
+			log.Printf("s.UserChannelCreate failed to create DM for %s: %s",
+				msg.Author.Username, err)
+			return
+		}
+		_, err = s.ChannelMessageSend(dm.ID, man.Meme)
+		if err != nil {
+			log.Printf("session.ChannelMessageSend failed: %s", err)
+		}
+	case "ping":
+		dm, err := s.UserChannelCreate(msg.Author.ID)
+		if err != nil {
+			log.Printf("s.UserChannelCreate failed to create DM for %s: %s",
+				msg.Author.Username, err)
+			return
+		}
+		_, err = s.ChannelMessageSend(dm.ID, man.Ping)
+		if err != nil {
+			log.Printf("session.ChannelMessageSend failed: %s", err)
+		}
+	case "rule34":
+		dm, err := s.UserChannelCreate(msg.Author.ID)
+		if err != nil {
+			log.Printf("s.UserChannelCreate failed to create DM for %s: %s",
+				msg.Author.Username, err)
 			return
 		}
 		_, err = s.ChannelMessageSend(dm.ID, man.Rule34)
